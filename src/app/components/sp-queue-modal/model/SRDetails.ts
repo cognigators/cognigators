874a08f0c97ccfcb735175718bba0e1192aaddc3
Amitabha.Ghosh@vixxo.com <Amitabh.Ghosh@vixxo.com>
2021-09-27 16:43:32 +0530

    added header,footer,logo,srdetail class

---
+++ src/app/components/sp-queue-modal/model/SRDetails.ts
@@ -0,0 +1,14 @@
+export class SrDetails
+{
+  public select : boolean = true
+  public sr_id : string
+  public sp_id : string
+  public sp_name : string
+  public sp_rating : string
+  public misc_cost : string
+  public s_category_name : string
+  public s_subcategory_name : string
+  public time_in : string
+  public time_out : string
+  public totalCost : string
+}
